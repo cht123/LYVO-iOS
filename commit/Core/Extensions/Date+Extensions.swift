8d1620a72ddd8c782a0cebc912ee5377df451f30
Michael Taylor <michaelxtaylor@icloud.com>
2025-12-21 14:03:54 -0600

    test for xcode commit 2

--- commit/Core/Extensions/Date+Extensions.swift
+++ commit/Core/Extensions/Date+Extensions.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 extension Date {
-    /// Start of day for this date - test
+    /// Start of day for this date - test number 2
     var startOfDay: Date {
         Calendar.current.startOfDay(for: self)
     }
